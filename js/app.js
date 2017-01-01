5cf4726319eac7926973eee6157796d4ef5cdf1f
Heru Sularto (eR) <herusularto@gmail.com>
2017-01-02 04:40:25 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -345,5 +345,11 @@
     app.saveSelectedCities();
   }
 
-  // TODO add service worker code here
+   // TODO add service worker code here
+   if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+  
 })();
