0637248a5675bf473ead06cea3d98ef285b1bc8e
Heru Sularto (eR) <herusularto@gmail.com>
2017-01-02 04:33:57 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -50,12 +50,15 @@
     var selected = select.options[select.selectedIndex];
     var key = selected.value;
     var label = selected.textContent;
-    // TODO init the app.selectedCities array here
+    if (!app.selectedCities) {
+      app.selectedCities = [];
+    }
     app.getForecast(key, label);
-    // TODO push the selected city to the array and save here
+    app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
-
+  
   document.getElementById('butAddCancel').addEventListener('click', function() {
     // Close the add new city dialog
     app.toggleAddDialog(false);
